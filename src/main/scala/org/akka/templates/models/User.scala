9a032a899f26155487a196b8a630b4213f3de470
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2017-11-15 16:14:21 -0200

    Improve User.scala

--- src/main/scala/org/akka/templates/models/User.scala
+++ src/main/scala/org/akka/templates/models/User.scala
@@ -1,29 +1,52 @@
 package org.akka.templates.models
 
-import com.fasterxml.jackson.annotation.JsonIgnore
+import org.bson.types.ObjectId
 import org.mongodb.scala.bson.ObjectId
 
-import scala.annotation.meta.field
+case class FindByIdRequest(id: String) {
+  require(ObjectId.isValid(id), "the informed id is not a representation of a valid hex string")
+}
 
-/**
-  * @author Rodrigo Oler <roodrigoprogrammer@gmail.com>
-  * @param id
-  */
-case class UserRequest(id: String)
+case class UserResource(
+  id: String,
+  username: String,
+  email: String,
+  password: String,
+  firstName: String,
+  lastName: String,
+  age: Int) {
+  require(username != null, "username not informed")
+  require(email != null, "email not informed")
+  require(password != null, "password not informed")
+  require(firstName != null, "firstName not informed")
+  require(lastName != null, "lastName not informed")
+  require(username.nonEmpty, "username cannot be empty")
+  require(email.nonEmpty, "email not informed")
+  require(password.nonEmpty, "password not informed")
+  require(firstName.nonEmpty, "firstName not informed")
+  require(lastName.nonEmpty, "lastName not informed")
+  require(age > 0, "age cannot be lower than 1")
 
-/**
-  * @author Rodrigo Oler <roodrigoprogrammer@gmail.com>
-  * @param _id
-  * @param username
-  * @param age
-  */
-case class User(@(JsonIgnore@field)
-                var _id: ObjectId,
-                username: String,
-                email: String,
-                password: String,
-                firstName: String,
-                lastName: String,
-                age: Int) {
-  def id: String = _id.toString
+  def asDomain = User(
+    if (id == null) ObjectId.get()
+    else new ObjectId(id), username, email, password, firstName, lastName, age)
+}
+
+case class User(
+                 _id: ObjectId,
+                 username: String,
+                 email: String,
+                 password: String,
+                 firstName: String,
+                 lastName: String,
+                 age: Int) {
+  def asResource = UserResource(
+    _id.toHexString,
+    username,
+    email,
+    password,
+    firstName,
+    lastName,
+    age
+  )
 }
